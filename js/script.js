e5adfa3aa7d5ff5e29d92af0498c234fe839b65e
Jay McArthur <Jay.mcalex@gmail.com>
2022-06-14 08:38:42 -0600

    Branch Commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Used to toggle the menu on small screens when clicking on the menu button
+function toggleMenu() {
+  var x = document.getElementById("navDemo");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+// When the user clicks anywhere outside of the modal, close it
+var modal = document.getElementById('ticketModal');
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
