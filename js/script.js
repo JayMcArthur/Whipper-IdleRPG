5941df28828a7753c5b6e4728335cd7cdbf807c3
Jay McArthur <Jay.mcalex@gmail.com>
2022-06-16 14:45:56 -0600

    Page Cleanup

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Used to toggle the menu on small screens when clicking on the menu button
 function toggleMenu() {
   var x = document.getElementById("navDemo");
-  if (x.className.indexOf("w3-show") == -1) {
+  if (x.className.indexOf("w3-show") === -1) {
     x.className += " w3-show";
   } else { 
     x.className = x.className.replace(" w3-show", "");
@@ -11,7 +11,7 @@ function toggleMenu() {
 // When the user clicks anywhere outside of the modal, close it
 var modal = document.getElementById('ticketModal');
 window.onclick = function(event) {
-  if (event.target == modal) {
+  if (event.target === modal) {
     modal.style.display = "none";
   }
 }
